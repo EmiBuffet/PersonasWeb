1014e7d0f21a944a64e008003940f317d3970953
EmiBuffet <emibuffet@gmail.com>
2023-04-12 19:07:19 -0300

    Agregar archivos de proyecto.

---
+++ PersonasWeb/Controllers/PersonaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace PersonasWeb.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // URL: /Persona
+        public string Index()
+        {
+            return "Crear Persona";
+        }
+        // URL: /Persona/CrearPersona
+        public string CrearPersona()
+        {
+            return "Pantalla para crear personas";
+        }
+
+        // URL: /Persona/ConsultarPersona?parametro=
+        public string ConsultarPersona(string parametro)
+        {
+            return HtmlEncoder.Default.Encode($"{parametro}");
+        }
+
+        //Modificar el metodo ConsultarPersona para que reciba dos parametros: nombre y apellido
+        //Y luego mostrar "Nombre: Emiliano, Apellido: Buffet"
+    }
+}
