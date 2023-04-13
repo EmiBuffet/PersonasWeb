d1cbb245f2fa6f17bd47016deb4596aeea03f7bb
EmiBuffet <emibuffet@gmail.com>
2023-04-13 19:19:22 -0300

    Clase 20230413

--- PersonasWeb/Controllers/PersonaController.cs
+++ PersonasWeb/Controllers/PersonaController.cs
@@ -11,17 +11,26 @@ namespace PersonasWeb.Controllers
             return "Crear Persona";
         }
         // URL: /Persona/CrearPersona
-        public string CrearPersona()
+        public IActionResult CrearPersona()
         {
-            return "Pantalla para crear personas";
+            return View();
+        }
+
+        // Ejercicio: Crear vista para constar personas, sin parametros
+        // Debe mostrar un h2 con el texto "Pantalla para consultar Persona"
+        // La misma se debe acceder desde una opcion en el menú
+        public IActionResult ConsultarPersona(string nombre)
+        {
+            ViewData["nombre"] = nombre;
+            return View();
         }
 
         // Recibe dos parametros nombre y apellido
         // Retorna un texto "Nombre: Emiliano, Apellido: Buffet"
         // URL: /Persona/ConsultarPersona?nombre=Emiliano&apellido=Buffet
-        public string ConsultarPersona(string nombre, string apellido)
+        /*public string ConsultarPersona(string nombre, string apellido)
         {
             return HtmlEncoder.Default.Encode($"Nombre: {nombre}, Apellido: {apellido}");
-        }
+        }*/
     }
 }
