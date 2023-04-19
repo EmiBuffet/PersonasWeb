5ec260f8a3ffee9ba22c6d8a254b5042ea8b0249
EmiBuffet <emibuffet@gmail.com>
2023-04-18 22:30:36 -0300

    Clase 20230418

---
+++ PersonasWeb/Migrations/20230419003545_AgregarCampos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonasWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "Persona",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Ciudad",
+                table: "Persona",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Persona",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Sexo",
+                table: "Persona",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "Persona");
+
+            migrationBuilder.DropColumn(
+                name: "Ciudad",
+                table: "Persona");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Persona");
+
+            migrationBuilder.DropColumn(
+                name: "Sexo",
+                table: "Persona");
+        }
+    }
+}
--- PersonasWeb/Models/Persona.cs
+++ PersonasWeb/Models/Persona.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace PersonasWeb.Models
 {
@@ -6,6 +7,32 @@ namespace PersonasWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+
+        [EnumDataType(typeof(Sexo))]
+        public Sexo Sexo { get; set; }
+
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [EnumDataType(typeof(Ciudad))]
+        public Ciudad Ciudad { get; set; }
+    }
+
+    public enum Sexo
+    {
+        Masculino = 1,
+        Femenino = 2,
+        Otro = 3
+    }
+
+    public enum Ciudad
+    {
+        Sunchales = 1,
+        Ataliva = 2,
+        Rafaela = 3
     }
 }
